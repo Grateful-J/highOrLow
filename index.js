a5b1910ad1a5bb5e7cd8e639466895658d0264ca
mrdj863 <107734167+mrdj863@users.noreply.github.com>
2023-10-05 19:44:17 -0400

    random URL functional

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
+require('dotenv').config();
+const axios = require('axios');
+
+
+//Deck of Cards API URL
 const deckUrl = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
 
-let gifSearch;
-let gifRating = ['g', 'pg', 'pg-13', 'r']
-const gifAPI = process.env.YOUR_API_KEY;
+//Giphy API search string variables
+let gifSearch = 'sad-dog';
+let gifRating = ['g', 'pg', 'pg-13', 'r'];
+const gifAPI = process.env.GIF_API_KEY;
 
+//Allows user to seach terms & "age" rating for returned content
+let gifUrl = `https://api.giphy.com/v1/gifs/random?api_key=${gifAPI}&tag=${gifSearch}&rating=${gifRating[2]}`;  
 
-let gifUrl = `https://api.giphy.com/v1/gifs/random?api_key=${gifAPI}&tag=${gifSearch}&rating=${gifRating}`
+let gotGifs;
 
+async function fetchGifs() {
+    axios.get(gifUrl)
+    .then(response => {
+        const gotGifs = response.data.data.url;
+        // sendGif(gotGifs)
+        console.log( gotGifs);
 
+        //console.log ('response.data dump below: ' , response.data)
+    })
+
+    .catch(error => {
+        console.log(`Error message: ${error.message}`)
+    })
+}
+
+fetchGifs()
+
+// Sends requested URL to front end
+// function sendGif(gotGifs) {
+//     document.getElementById('gifLink').textContent = gotGifs;
+//}
 
