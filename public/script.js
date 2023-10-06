1edaf546eda7ab9b6f74f128a9e3b682253c0edd
mrdj863 <107734167+mrdj863@users.noreply.github.com>
2023-10-06 13:24:31 -0400

    adding value check formula for high/low

--- public/script.js
+++ public/script.js
@@ -1,5 +1,81 @@
 document.getElementById('gifImage').onerror = handleImageError;
 
+let computerCard;
+let secondCard;
+
+//determines card ranking
+function valueCheck() {
+    //Object of card  values
+    const cardValues = {
+        '1': 1,
+        '2': 2,
+        '3': 3,
+        '4': 4,
+        '5': 5,
+        '6': 6,
+        '7': 7,
+        '8': 8,
+        '9': 9,
+        '0': 10,
+        'J': 11,
+        'Q': 12,
+        'K': 13,
+        'A': 14
+    };
+
+    const computerValue = cardValues[computerCard[0]];
+    const secondValue = cardValues[secondCard[0]];
+
+    if (computerValue < secondValue) {
+        return 'higher';
+    } else if (computerValue > secondValue){
+        return 'lower'
+    } else {
+        return 'draw'
+    }
+}
+
+
+//ToDo: fetches from /shuffleCards
+
+
+
+//ToDo: Deal Function
+function dealCard() {
+    //fetches from /drawcard
+    //Example return of /drawCard
+    //{"card":"8C","img":"https://deckofcardsapi.com/static/img/8C.png"}
+
+    const cardImage = dealCards(response).img
+
+    const newCard = document.getElementById('gifImage');
+    newCard.src = cardImage
+
+    //assign computerCard a value
+    ComputerCard = response.card.value[0] //ignoring suit
+
+}
+
+//ToDo: Choose Lower Function
+function chooseLower() {
+    //deal second card
+    //check if card is lower 
+    chooseHigher(computerCard,secondCard);
+    if (chooseHigher === "lower"){
+        fetchGif() // ToDo isntead of searching for gif, present an array to pull from of wining gifs
+    } // ToDo: else statment with Losing gifs for lose or draw
+}
+
+//ToDo Choose Higher Function
+function chooseHigher(computerCard, secondCard) {
+    //ToDo: rinse and repeast from above function
+}
+
+
+
+
+
+
 
 //Fetches GIF from Giphy
 function fetchGif() {
--- server.js
+++ server.js
@@ -11,20 +11,42 @@ app.listen(PORT, () => {
 
 let deckID;
 let drawCount = 1;
+let playerCard;
 
 //Deck of Cards API URL
 const shuffleUrl = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
-const drawUrl = `https://www.deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`
+
+//requests a new deck and stores deck value for later draws
 
 app.get('/shuffleDeck', async (req, res) => {
     try {
-        const response = await axios.get(deckUrl);
+        const response = await axios.get(shuffleUrl);
         deckID = response.data.deck_id;
-        res.json({ deck_id: deckID});
+        console.log(deckID)
+        res.json({ deck_id: deckID,});
+        return deckID;
     }
     catch(error) {
         console.log(`Error shuffling deck`, error);
         res.status(500).json({error: 'Failed shuffling new deck'})
+    }
+})
+
+
+
+//Uses deck value to call for a new card
+app.get('/drawCard', async(req, res) => {
+    const drawUrl = `https://www.deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`
+    try {
+        const response = await axios.get(drawUrl);
+        playerCard = response.data.cards[0].code; // temp stores card for later calls
+        cardImage = response.data.cards[0].image;
+        res.json({card: playerCard, img: cardImage })
+    }
+    
+    catch(error) {
+        console.log('Error drawing card');
+        res.status(500).json({error: 'Failed drawing card'})
     }
 })
 
