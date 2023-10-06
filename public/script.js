e35170ba19db02dd85ac04137e92ce5f862b8ab9
mrdj863 <107734167+mrdj863@users.noreply.github.com>
2023-10-06 08:31:57 -0400

    gif search working now

--- public/script.js
+++ public/script.js
@@ -1,4 +1,29 @@
+document.getElementById('gifImage').onerror = handleImageError;
+
+
+
 function fetchGif() {
-    const searchTerm = document.getElementById('searchTerm').ariaValueMax;
-    const gifUrl = ''
+    const searchTerm = document.getElementById('searchTerm').value;
+    const gifUrl = `/getGif?search=${searchTerm}`
+
+    //Fetch the GIF
+    fetch(gifUrl)
+    .then(response => response.json())
+    .then(data => {
+        //displays the fetched GIF on front end
+        console.log('uploading URL now')
+        const gifImage = document.getElementById('gifImage');   
+        gifImage.src = data.url;
+
+
+    })
+
+    .catch(error => {
+        console.log(`Error fetching Gif`, error)
+    })
+}
+
+function handleImageError() {
+    const gifImage = document.getElementById('gifImage');
+    console.error("Error loading image:", gifImage.src);
 }
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.static('public'))
+
 //Deck of Cards API URL
 const deckUrl = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
 
@@ -13,11 +15,13 @@ app.get('/getGif', async (req, res) => {
     const gifSearch = req.query.search;
     const gifAPI = process.env.GIF_API_KEY;
     let gifRating = ['g', 'pg', 'pg-13', 'r'];
-    let gifUrl = `https://api.giphy.com/v1/gifs/random?api_key=${gifAPI}&tag=${gifSearch}&rating=${gifRating[2]}`;
+    let gifUrl = `https://api.giphy.com/v1/gifs/random?api_key=${gifAPI}&tag=${gifSearch}&rating=${gifRating[3]}`;
 
     try {
         const response = await axios.get(gifUrl);
-        res.json({url: response.data.data.url});
+        const directGifUrl = response.data.data.bitly_gif_url;
+        res.json({url: response.data.data.images.original.url});
+
     }
 
     catch(error) {
